b5f072bdd2b7afb3f250099750973cba30501a27
natolay <ndixon@seattleu.edu>
2022-10-16 23:40:05 +0200

    added containers for icon and information

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+let pokemoniconContainerEl = document.querySelector("#pokemonicon-container");
 let pokemonContainerEl = document.querySelector("#pokemon-container");
+
+
 
 function getPokemon(){
 
@@ -14,16 +17,49 @@ function getPokemon(){
 
             let pokemonName = document.createElement("h3");
             let pokemonID = document.createElement("h2");
+            let pokemonbaseex = document.createElement("p");
+            let pokemonheight = document.createElement("p");
+            let pokemonweight = document.createElement("p");
+            let pokemontype = document.createElement("p");
+            let pokemonability = document.createElement("p");
+            let pokemonhp = document.createElement("p");
+            let pokemonattack = document.createElement("p");
+            let pokemondefense = document.createElement("p");
+           
             let pokemonSprite = document.createElement("img");
 
             pokemonName.textContent = data.name;
-            pokemonID.textContent = data.id;
+            pokemonID.textContent = `#${data.id}`
+            pokemonbaseex.textContent = `Base Experience: ${data.base_experience}`
+            pokemonheight.textContent = `Height: ${data.height} meter(s)`
+            pokemonweight.textContent = `Weight: ${data.weight} kilogram(s)`
+            pokemontype.textContent = `Type: ${data.types[0].type.name}`
+            pokemonability.textContent = `Ability: ${data.abilities[0].ability.name}, ${data.abilities[1].ability.name}`
+            pokemonhp.textContent = `HP: ${data.stats[0].base_stat}`
+            pokemonattack.textContent = `Attack: ${data.stats[1].base_stat}`
+            pokemondefense.textContent = `Defense: ${data.stats[2].base_stat}`
+
+            
+
             pokemonSprite.setAttribute("src", data.sprites.front_default)
 
-        
+           
+
+
+
             pokemonContainerEl.appendChild(pokemonName)
             pokemonContainerEl.appendChild(pokemonID)
-            pokemonContainerEl.appendChild(pokemonSprite)
+            pokemonContainerEl.appendChild(pokemonbaseex)
+            pokemonContainerEl.appendChild(pokemonheight)
+            pokemonContainerEl.appendChild(pokemonweight)
+            pokemonContainerEl.appendChild(pokemontype)
+            pokemonContainerEl.appendChild(pokemonability)
+            pokemonContainerEl.appendChild(pokemonhp)
+            pokemonContainerEl.appendChild(pokemonattack)
+            pokemonContainerEl.appendChild(pokemondefense)
+            pokemoniconContainerEl.appendChild(pokemonSprite)
+
+            
         })
 
 
